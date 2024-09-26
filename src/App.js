1a263a36ecb4cfbccf04b6382e05850e9feb85be
Nikita Petrenko <tebene44@gmail.com>
2024-09-26 16:55:01 +1000

    v1

---
+++ src/App.js
@@ -0,0 +1,25 @@
+﻿import { useEffect } from 'react';
+import './App.css';
+
+const tg = window.Telegram.WebApp;
+function App() {
+
+    useEffect(() => {
+        tg.ready();
+    }, [])
+
+    const onClose = () => {
+        tg.close()
+    }
+
+  return (
+    <div className="App">
+          work
+          <button onClick={onclose}> Закрыть</button>
+    </div>
+  );
+}
+
+
+
+export default App;
